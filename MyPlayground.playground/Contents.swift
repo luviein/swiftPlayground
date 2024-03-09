31be11d838af591c76aed5fff90db474de707cbb
luviein <yenlengggggg@gmail.com>
2024-03-09 23:50:41 +0800

    Add checkpoint 5 and playground edits

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,22 @@
 import Cocoa
+//
+//let awesomeTalk = {
+//    print("Here's a great talk!")
+//}
+//func deliverTalk(name: String, type: () -> Void) {
+//    print("My talk is called \(name)")
+//    type()
+//}
+//deliverTalk(name: "My Awesome Talk", type: awesomeTalk)
 
-for i in 1...100 {
-    print("Counting from 1 - 100 : \(i)")
+func repeatAction(count: Int, action: () -> Void) {
+    for _ in 0..<count {
+        action()
+    }
+}
+repeatAction(count: 5) {
+    print("Hello, world!")
+}
+repeatAction(count: 3) {
+    print("Repeating")
 }
---
+++ checkpoint5_closures.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Cocoa
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+let result = luckyNumbers
+    .filter{$0 / 2 != 0}
+    .sorted()
+    .map{"\($0) is a lucky number"}
+
+result.forEach{print($0)}
